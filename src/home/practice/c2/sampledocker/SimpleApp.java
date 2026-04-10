f17f7cf21db16d9237fbe186b131a3fa0812153e
jagadeeshvelmurugan <jagadeesh.velmurugan@gmail.com>
2026-04-11 02:24:55 +0530

    second commit after a long time

---
+++ src/home/practice/c2/sampledocker/SimpleApp.java
@@ -0,0 +1,7 @@
+package home.practice.c2.sampledocker;
+
+public class SimpleApp {
+    public static void main(String[] args) {
+        System.out.println("This is my first and many more programs to come after 18 years ! I Miss you Java to the core and love you to the Moon & Back :)");
+    }
+}
